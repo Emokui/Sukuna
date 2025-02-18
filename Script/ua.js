e23d2d595471e3bfd4bcec2eca8869abc0da9132
Megumi <136175327+Emokui@users.noreply.github.com>
2025-02-18 15:07:40 +0800

    Delete Script/ua.js

--- Script/ua.js
+++
@@ -1,8 +0,0 @@
-// 修改请求的 User-Agent
-if ($request) {
-  $done({
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1'
-    }
-  });
-}
