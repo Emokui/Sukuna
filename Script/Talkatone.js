ec1ab0a65b72e6d619327075cb11ca2710b65b25
Megumi <136175327+Emokui@users.noreply.github.com>
2025-03-08 14:36:36 +0800

    Delete Script/Talkatone.js

--- Script/Talkatone.js
+++
@@ -1,9 +0,0 @@
-let body = $response.body;
-
-body = body.replace(/<div id="top-ad-banner".*?<\/div>/g, '');
-
-body = body.replace(/<div id="middle-ad-banner".*?<\/div>/g, '');
-
-body = body.replace(/<div id="bottom-ad-banner".*?<\/div>/g, '');
-
-$done({ body });
